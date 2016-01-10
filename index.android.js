ad97a71b8f8428d7b68dfb67937d23da55666673
Ramon Gebben <ramon@ra-ge.net>
2016-01-10 12:38:27 +0100

    search algorithem working

--- index.android.js
+++ index.android.js
@@ -10,21 +10,86 @@ var {
   StyleSheet,
   Text,
   View,
+  ScrollView
 } = React;
 
 var getMeHighMobile = React.createClass({
+
+  getInitialState: function(){
+      return {
+          shops: [
+              {name: 'Loading shops', id: '1234'}
+          ],
+          lastPosition: 'unknown',
+          theOne: 'unknown'
+    }
+  },
+  componentDidMount: function(){
+      fetch('http://192.168.2.6:3000/shops')
+        .then(res => res.json())
+        .then(shops =>{
+            this.setState({ shops });
+            console.log(shops);
+        });
+        this._getGeoLocation();
+  },
+
+  componentWillUnmount: function() {
+      navigator.geolocation.clearWatch(this.watchID);
+  },
+  _getGeoLocation: function(){
+      this.watchID = navigator.geolocation.watchPosition((position) => {
+          this.setState({lastPosition: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+          }});
+          this._getMeHigh()
+      });
+  },
+  _getMeHigh: function(){
+      this.findShop((theOne => {
+        console.log(theOne);
+        this.setState({
+            theOne: theOne
+        });
+      }));
+  },
+  getDistance: function(p1, p2) {
+      // http://www.wikiwand.com/en/Haversine_formula
+      const radius = x => x * Math.PI / 180;
+
+      var R = 6378137,
+          dLat = radius( p2.lat - p1.lat ),
+          dLong = radius( p2.lng - p1.lng ),
+          a = Math.sin( dLat / 2 ) * Math.sin( dLat / 2 ) + Math.cos( radius( p1.lat ) ) * Math.cos( radius( p2.lat ) ) * Math.sin( dLong / 2 ) * Math.sin( dLong / 2 ),
+          c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return R * c;
+  },
+  findShop: function(holaback){
+      let theOne = {};
+      this.state.shops.forEach(( shop ) => {
+          shop.distance = this.getDistance( this.state.lastPosition, shop.location );
+          if( shop.distance <= theOne.distance || theOne.distance === undefined ){
+              theOne = shop;
+          }
+      });
+      if(holaback) holaback(theOne);
+  },
   render: function() {
+    var shops = this.state.shops.map((shop) => {
+        return(<Text key={shop.id} style={styles.instructions}>{shop.name}</Text>)
+    });
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+      <View>
+          <Text>location: {JSON.stringify(this.state.lastPosition)}</Text>
+          <Text>TheOne: {JSON.stringify(this.state.theOne)}</Text>
+          <ScrollView
+            automaticallyAdjustContentInsets={false}
+            scrollEventThrottle={200}
+            style={styles.scrollView}>
+            {shops}
+          </ScrollView>
       </View>
     );
   }
@@ -41,6 +106,9 @@ var styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  scrollView: {
+      height: 300,
   },
   instructions: {
     textAlign: 'center',
