eb6b06ce40c252f3f5711a3831bb3035fb18568e
Ramon Gebben <ramon@ra-ge.net>
2016-01-14 10:42:30 +0100

    everything now

--- index.android.js
+++ index.android.js
@@ -5,6 +5,7 @@
 'use strict';
 
 var React = require('react-native');
+var RNGMap = require('react-native-gmaps');
 var {
   AppRegistry,
   StyleSheet,
@@ -87,6 +88,28 @@ var getMeHighMobile = React.createClass({
           <Text>location: {JSON.stringify(this.state.lastPosition)}</Text>
           <Text>TheOne: {JSON.stringify(this.state.theOne)}</Text>
           {gmhButton}
+          <RNGMap
+            ref={'gmap'}
+            style={ { height: 500, width: 500 } }
+            markers={ [
+                  { coordinates: {lng: 0.1, lat: 51.0} },
+                  {
+                    coordinates: {lng: -0.1, lat: 51.0},
+                    title: "Click marker to see this title!",
+                    snippet: "Subtitle",
+                    id: 0,
+                    color: 120,
+                  }
+              ] }
+            zoomLevel={10}
+            onMapChange={(e) => console.log(e)}
+            onMapError={(e) => console.log('Map error --> ', e)}
+            center={ { lng: 0.1, lat: 51.0 } }
+            /*
+             * clickMarker shows Info Window of Marker with id: 0,
+             * hides Info Window if given null
+             */
+            clickMarker={0}/>
       </View>
     );
   }
