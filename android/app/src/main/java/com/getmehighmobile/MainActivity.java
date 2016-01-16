c3ad6525ce6b0d03eac80d11dc3fbeadfb731b51
Ramon Gebben <ramon@ra-ge.net>
2016-01-16 11:19:15 +0100

    importing without errors

--- android/app/src/main/java/com/getmehighmobile/MainActivity.java
+++ android/app/src/main/java/com/getmehighmobile/MainActivity.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
 import com.rota.rngmaps.RNGMapsPackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.facebook.soloader.SoLoader;
 
 public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
@@ -28,6 +29,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
                 .setJSMainModuleName("index.android")
                 .addPackage(new MainReactPackage())
                 .addPackage(new RNGMapsPackage())
+                .addPackage(new ReactMaterialKitPackage())
                 .setUseDeveloperSupport(BuildConfig.DEBUG)
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
